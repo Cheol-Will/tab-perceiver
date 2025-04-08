2ecf3d03e6e263a5885651e076f3a38963cd4d48
Cheol-Will <kangcs7083@naver.com>
2025-04-08 17:45:07 +0900

    first commit

--- benchmark/load_result.py
+++
@@ -1,9 +0,0 @@
-
-from utils import load_result, plot_result
-
-def main():
-    result_list, task_type = load_result()
-    plot_result(result_list, task_type)
-
-if __name__ == "__main__":
-    main()
