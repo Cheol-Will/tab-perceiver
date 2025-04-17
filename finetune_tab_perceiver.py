9826ca855ff381e4a13e2c317c791e4143439963
Cheol-Will <kangcs7083@naver.com>
2025-04-17 09:34:36 +0900

    define train and test function:

--- finetune_tab_perceiver.py
+++ finetune_tab_perceiver.py
@@ -6,15 +6,13 @@ import time
 from typing import Any, Optional
 
 import numpy as np
-import optuna
 import torch
-from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, Module, MSELoss
+from torch.nn import CrossEntropyLoss, Module
 from torch.nn import LayerNorm, Linear, Sequential, Parameter
 from torch.optim.lr_scheduler import ExponentialLR
-from torchmetrics import AUROC, Accuracy, MeanSquaredError
+from torchmetrics import Accuracy, Metric
 from tqdm import tqdm
 
-from torch_frame import stype
 from torch_frame import TensorFrame, stype
 from torch_frame.typing import TaskType
 from torch_frame.data import DataLoader
@@ -26,8 +24,6 @@ from torch_frame.nn.encoder.stype_encoder import (
 from torch_frame.nn.encoder.stypewise_encoder import StypeWiseFeatureEncoder
 from models import TabPerceiver
 
-from data_frame_benchmark import train, test
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--task_type', type=str, choices=[
@@ -37,30 +33,19 @@ parser.add_argument(
     ], default='binary_classification')
 parser.add_argument('--scale', type=str, choices=['small', 'medium', 'large'],
                     default='small')
-parser.add_argument('--idx', type=int, default=0,
-                    help='The index of the dataset within DataFrameBenchmark')
 parser.add_argument('--epochs', type=int, default=50)
-parser.add_argument('--num_trials', type=int, default=20,
-                    help='Number of Optuna-based hyper-parameter tuning.')
-parser.add_argument(
-    '--num_repeats', type=int, default=5,
-    help='Number of repeated training and eval on the best config.')
-parser.add_argument(
-    '--model_type', type=str, default='TabNet', choices=[
-        'TabNet', 'FTTransformer', 'ResNet', 'MLP', 'TabTransformer', 'Trompt',
-        'ExcelFormer', 'FTTransformerBucket', 'XGBoost', 'CatBoost', 'LightGBM',
-        'TabPerceiver'
-    ])
 parser.add_argument('--seed', type=int, default=0)
 parser.add_argument('--result_path', type=str, default='')
 args = parser.parse_args()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+loss_fun = CrossEntropyLoss()
 torch.manual_seed(args.seed)
 
+
 def build_loader(args, dataset_index):
+    print(f"Start building dataloder for data_{args.scale}_{dataset_index}")
     path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
-    # For multiclass_classification, there exist three datsets.
     dataset = DataFrameBenchmark(root=path, task_type=TaskType(args.task_type),
                                 scale=args.scale, idx=dataset_index)
     dataset.materialize()
@@ -79,29 +64,81 @@ def build_loader(args, dataset_index):
     valid_loader = DataLoader(val_tensor_frame, batch_size=128)
     test_loader = DataLoader(test_tensor_frame, batch_size=128)
 
+    print('Training set has {} instances'.format(len(train_tensor_frame)))
+    print('Validation set has {} instances'.format(len(val_tensor_frame)))
+    print('Test set has {} instances'.format(len(test_tensor_frame)))
+
     return train_loader, valid_loader, test_loader, col_stats, col_names_dict, num_classes
+
+def train(
+    model: Module,
+    loader: DataLoader,
+    optimizer: torch.optim.Optimizer,
+    epoch: int,
+) -> float:
+    model.train()
+    loss_accum = total_count = 0
+
+    for tf in tqdm(loader, desc=f'Epoch: {epoch}'):
+        tf = tf.to(device)
+        y = tf.y
+        pred = model(tf)
+
+        if pred.size(1) == 1:
+            pred = pred.view(-1, )
+        loss = loss_fun(pred, y)
+        optimizer.zero_grad()
+        loss.backward()
+        loss_accum += float(loss) * len(tf.y)
+        total_count += len(tf.y)
+        optimizer.step()
+    return loss_accum / total_count
+
+
+def test(
+    model: Module,
+    loader: DataLoader,
+    metric_computer: Metric,
+) -> float:
+    model.eval()
+    metric_computer.reset()
+    for tf in loader:
+        tf = tf.to(device)
+        pred = model(tf)
+        pred = pred.argmax(dim=-1)
+        metric_computer.update(pred, tf.y)
+    return metric_computer.compute().item()
+
 
 def main(args):
     num_heads = 8
-    hidden_dim = 256
     num_layers = 6
     num_latents = 32
     hidden_dim = 256
     dropout_prob = 0.2
 
+    result_dicts = {}
+    # For multiclass_classification, there exist three datsets.
     for i in range(0, 3):
         train_loader, valid_loader, test_loader, col_stats, col_names_dict, num_classes = build_loader(args, i)
+        num_features = 0
+        metric_computer = Accuracy(task='multiclass',
+                                    num_classes=num_classes).to(device)
+        for k, v in col_names_dict.items():
+            num_features += len(v)    
+
         if i == 0:
             model = TabPerceiver(
-                num_classes,
-                num_heads,
-                num_layers,
-                num_latents,
-                hidden_dim,
-                dropout_prob,
+                out_channels=num_classes,
+                num_features=num_features,
+                num_heads=num_heads,
+                num_layers=num_layers,
+                num_latents=num_latents,
+                hidden_dim=hidden_dim,
+                dropout_prob=dropout_prob,
                 col_stats=col_stats,
                 col_names_dict=col_names_dict
-            )
+            ).to(device)
         else:
             # freeze
             for param in model.parameters():
@@ -118,9 +155,11 @@ def main(args):
                 col_names_dict=col_names_dict,
                 stype_encoder_dict=stype_encoder_dict,
             )
+            # positional encoding for embedding
+            model.pos_embedding = Parameter(torch.empty(1, num_features, hidden_dim))
+
             # make new decoder query
             model.queries = Parameter(torch.empty(1, 1, hidden_dim))
-            torch.nn.init.trunc_normal_(model.queries, std=0.02)
             
             # make new prediction head
             model.proj = Sequential(
@@ -128,11 +167,14 @@ def main(args):
                 Linear(hidden_dim, num_classes)
             )           
 
+            # initialize pos_embedding and queries
+            model.reset_parameters_finetune()
+
         # check if new layers are requires_grad = True
-        # print(f"training {i}th dataset ")
-        # for name, params in model.named_parameters():
-        #     if params.requires_grad:
-        #         print(name)
+        print(f"training {i}th dataset ")
+        for name, params in model.named_parameters():
+            if params.requires_grad:
+                print(name)
 
         optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
         lr_scheduler = ExponentialLR(optimizer, gamma=0.9)
@@ -140,18 +182,27 @@ def main(args):
         best_val_metric = math.inf
         best_test_metric = math.inf
         
-        for epoch in range(1, args.epoch + 1):
+        for epoch in range(1, args.epochs + 1):
             train_loss = train(model, train_loader, optimizer, epoch)
-            val_metric = test(model, valid_loader)
+            val_metric = test(model, valid_loader, metric_computer)
 
             if val_metric < best_val_metric:
                 best_val_metric = val_metric
-                best_test_metric = test(model, test_loader)
+                best_test_metric = test(model, test_loader, metric_computer)
             lr_scheduler.step()
             print(f'Train Loss: {train_loss:.4f}, Val: {val_metric:.4f}')
 
         print(f'Best val: {best_val_metric:.4f}, Best test: {best_test_metric:.4f}')
-
+        data_name = f"{args.scale}_{i}"
+        result_dict = {
+            "args": args.__dict__, # same for all dataset
+            'best_val_metric': best_val_metric,
+            'best_test_metric': best_test_metric,
+        }
+        result_dicts[data_name] = result_dict
+    if args.result_path != '':
+        os.makedirs(os.path.dirname(args.result_path), exist_ok=True)
+        torch.save(result_dicts, args.result_path)
 
 if __name__ == '__main__':
     main(args)
