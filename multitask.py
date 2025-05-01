c990fc70ef75f4531a35a9b1efa89dc60d1822b1
Cheol-Will <kangcs7083@naver.com>
2025-05-01 20:39:36 +0900

    add finetune and eval

--- multitask.py
+++ multitask.py
@@ -17,8 +17,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def shuffle_task_index(dataloaders):
     task_order = []
-    for taks_index, dataloader in enumerate(dataloaders):
-        task_order += [taks_index] * len(dataloader)
+    for task_idx, dataloader in enumerate(dataloaders):
+        task_order += [task_idx] * len(dataloader)
     random.shuffle(task_order)
     return task_order
 
@@ -51,8 +51,7 @@ def train(
         loss_accum += float(loss) * len(tf.y)
         total_count += len(tf.y)
         optimizer.step()
-    return loss_accum / total_count
-
+    return loss_accum / total_count       
 
 def test(
     model: Module,
@@ -63,19 +62,45 @@ def test(
 ) -> float:
     model.eval()
     metric_computer.reset()
-    for tf in loader:
-        tf = tf.to(device)
-        pred = model(tf, task_idx)
-        if task_type == TaskType.MULTICLASS_CLASSIFICATION:
-            pred = pred.argmax(dim=-1)
-        elif task_type == TaskType.REGRESSION:
-            pred = pred.view(-1, )
-        metric_computer.update(pred, tf.y)
+    with torch.no_grad():
+        for tf in loader:
+            tf = tf.to(device)
+            pred = model(tf, task_idx)
+            if task_type == TaskType.MULTICLASS_CLASSIFICATION:
+                pred = pred.argmax(dim=-1)
+            elif task_type == TaskType.REGRESSION:
+                pred = pred.view(-1, )
+            metric_computer.update(pred, tf.y)
     return metric_computer.compute().item()
+
+def finetune_and_eval(
+    model: Module,
+    train_loaders,
+    test_loaders,
+    optimizer,
+    lr_scheduler,
+    loss_fun,
+    finetune_epoch,
+    metric_computer: Metric,
+    task_type,
+    task_idx,
+) -> float:
+
+    # create task_idx_list for one task
+    finetune_task_idx_list = [task_idx] * len(train_loaders[task_idx])
+
+    # train on finetune epoch
+    for epoch in range(finetune_epoch):
+        train_loss = train(model, train_loaders, optimizer, loss_fun, epoch, task_type, finetune_task_idx_list)
+        lr_scheduler.step()
+        print(f"Train Loss: {train_loss:.7f}")
+
+    test_metric = test(model, test_loaders[task_idx], metric_computer, task_type, task_idx)
+    return test_metric
 
 def main(args):
     torch.manual_seed(args.seed)
-    result_list = []
+    random.seed(args.seed)
     model_config = {
         "num_heads": 8,
         "num_layers": 8,
@@ -113,10 +138,49 @@ def main(args):
                 "best_test_metric": test_metric
             }
         })                
-
+    # Save weight and result before fintune
+    checkpoint = {
+        "args": args.__dict__,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "scheduler_state_dict": lr_scheduler.state_dict(),
+        "best_test_metric": result_list,
+    }
     os.makedirs(os.path.dirname(args.result_path), exist_ok=True)
-    torch.save(result_list, args.result_path)
-    print(f"Result_list is saved into {args.result_path}")
+    torch.save(checkpoint, args.result_path) # Checkpoint before finetune
+    print(f"Checkpoint before fintune is saved into {args.result_path}")
+
+    # Finetune and eval for each task
+    finetune_result_list = []
+    for task_idx in range(num_tasks):
+        # load state_dict to finetune each task from the same weight
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        lr_scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
+        
+        finetune_test_metric = finetune_and_eval(
+            model,
+            train_loaders,
+            test_loaders,
+            optimizer,
+            lr_scheduler,
+            loss_fun,
+            args.finetune_epochs,
+            metric_computer,
+            task_type,
+            task_idx
+        )
+
+        finetune_result_list.append({
+            f"{args.task_type}_{args.scale}_{task_idx}": {
+                "finetune_best_test_metric": finetune_test_metric
+            }
+        })        
+
+    checkpoint["finetune_test_metric"] = finetune_result_list
+    torch.save(checkpoint, args.result_path)
+    print(f"Checkpoint after finetune is saved into {args.result_path}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -125,6 +189,7 @@ if __name__ == '__main__':
     parser.add_argument('--scale', type=str, choices=['small', 'medium', 'large'],
                         default='small')
     parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--finetune_epochs', type=int, default=5)
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--result_path', type=str, default='')
     args = parser.parse_args()
